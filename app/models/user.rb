a01be0f9e9eae3761501e7c75f1f4e856b5f77ac
Codrin Popescu <codrin@ascheri.co.uk>
2019-04-12 09:37:21 +0100

    styled log in view and msg display

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, 
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable  :registerable,
+  devise :database_authenticatable,
          :recoverable, :rememberable, :validatable
+
+   validates :email, presence: true, uniqueness: {case_sensitive: false}
 end
