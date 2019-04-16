a47e12fdce75cfc44df7ec8431af528a93f52ff3
Codrin Popescu <codrin@ascheri.co.uk>
2019-04-16 16:40:26 +0100

    added speakers and connected not and ascheri

--- app/controllers/webinars_controller.rb
+++ app/controllers/webinars_controller.rb
@@ -2,7 +2,7 @@ class WebinarsController < ApplicationController
   before_action :set_webinar, only: [:edit, :show, :update, :destroy]
 
   def index
-    @speaker = Speaker.all
+    
     @webinars = Webinar.paginate(page: params[:page], per_page: 10).order('created_at DESC')
   end
 
