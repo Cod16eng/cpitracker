54220c2c9572d0ef5f0b64417e8d7d4b1150bbe1
Codrin Popescu <codrin@ascheri.co.uk>
2019-04-17 09:20:48 +0100

    Add speaker and fix views

--- app/controllers/webinars_controller.rb
+++ app/controllers/webinars_controller.rb
@@ -2,7 +2,7 @@ class WebinarsController < ApplicationController
   before_action :set_webinar, only: [:edit, :show, :update, :destroy]
 
   def index
-    
+
     @webinars = Webinar.paginate(page: params[:page], per_page: 10).order('created_at DESC')
   end
 
@@ -33,7 +33,7 @@ class WebinarsController < ApplicationController
   end
 
   def show
-    @speaker = Speaker.find(params[:id])
+    
   end
 
   def destroy
